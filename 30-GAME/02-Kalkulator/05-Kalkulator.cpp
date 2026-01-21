9e2ab7634ddd4841d3a145d23619c0adcc99ebce
Mite <daviddwiwahyudi626@gmail.com>
2026-01-21 17:37:20 +0700

    kalkulator 5

---
+++ 30-GAME/02-Kalkulator/05-Kalkulator.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+int hitung(int nilai1, char op, int nilai2, bool &isError){
+    switch(op){
+        case '+': return nilai1+=nilai2;
+        case '-': return nilai1-=nilai2;
+        case '*': return nilai1*=nilai2;
+        case '/':
+            if(nilai2==0){
+                std::cout<<"Error: Pembagian nol!\n";
+                isError=true;
+                return nilai1;
+            }else if(nilai2!=0){
+                return nilai1/=nilai2;
+            }
+        case '%': return nilai1%=nilai2;
+        default:
+            std::cout<<"Operator salah!\n";
+            isError=true;
+            return nilai1;
+    }
+}
+
+int main(){
+
+    int nilai1, nilai2;
+    char op;
+    bool isError=false;
+    
+    std::cout<<"\nInput: ";
+    std::cin>>nilai1>>op>>nilai2;
+
+    nilai1=hitung(nilai1, op, nilai2, isError);
+
+    while(isError!=true){
+        std::cout<<"\nInput: "<<nilai1;
+        std::cin>>op>>nilai2;
+
+        nilai1=hitung(nilai1, op, nilai2, isError);
+    }
+    
+    return 0;
+}
