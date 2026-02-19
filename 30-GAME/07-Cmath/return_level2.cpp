8133cb58f361e3afaf06e161a00b2b7236840ffb
Mite <daviddwiwahyudi626@gmail.com>
2026-02-19 13:23:25 +0700

    return level 2

---
+++ 30-GAME/07-Cmath/return_level2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int multiplier(int level){
+    int level_mult;
+    level_mult = level * 10;
+    return level_mult;
+}
+
+// 1 koin emas setara 10 - 20 koin perak, jadi aku kalikan 10
+int jumlah(int gold_amount, int silver_amount){
+    int total_amount;
+    total_amount = (gold_amount * 10) + silver_amount;
+    return total_amount;
+}
+
+int main(){
+    int input_level, hasil, gold_amount, silver_amount, hasil2;
+
+    std::cout << "Masukkan level dasar karakter: ";
+    std::cin >> input_level;
+
+    hasil = multiplier(input_level);
+    std::cout << "Power Level (level dikali 10): " << hasil << std::endl;
+
+    // ================================================
+    std::cout << "\n";
+
+    std::cout << "Masukkan jumlah koin emas: ";
+    std::cin >> gold_amount;
+    std::cout << "Masukkan jumlah koin perak: ";
+    std::cin >> silver_amount;
+
+    hasil2 = jumlah(gold_amount, silver_amount);
+    std::cout << "Total kekayaan koin: " << hasil2 << std::endl;
+    
+    return 0;
+}
